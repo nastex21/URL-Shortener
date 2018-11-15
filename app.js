a3e7645611acaa12ca5f2ca8e4d56a3854498d71
Tony <nasax2000@gmail.com>
2018-11-15 05:49:36 -0600

    Commit

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+var dns = require('dns')
 const mongoose = require('mongoose');
 var cors = require('cors');
 var schema = mongoose.Schema;
 
 app.use(cors());
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -19,7 +21,10 @@ app.get('/', function(req, res){
 
 app.post('/api/shorturl/new', function(req, res){
     userUrl = req.body.url;
-    console.log(userUrl);
+    console.log(typeof userUrl);
+    var w3 = dns.lookup('freecodecamp.com', function (err, addresses, family) {
+        console.log(addresses);
+      });
 })
 
 mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true }); 
