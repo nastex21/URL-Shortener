1ea63edfd18351e912b029a0cafbfbd962789d98
nastex21 <nasax2000@gmail.com>
2018-11-15 20:15:46 -0600

    Fixed dns, address lookup validation
    -dns lookup works now
    -put in a function that returns a host name from a url address.

--- app.js
+++ app.js
@@ -11,35 +11,56 @@ var schema = mongoose.Schema;
 app.use(cors());
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/shorturl/new', function(req, res){
-    userUrl = req.body.url;
-    console.log(typeof userUrl);
-    var w3 = dns.lookup('freecodecamp.com', function (err, addresses, family) {
-        console.log(addresses);
-      });
-})
+function extractHostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
 
 mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true }); 
+  if (url.indexOf("//") > -1) {
+    hostname = url.split('/')[2];
+  } else {
+    hostname = url.split('/')[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+
+  return hostname;
+}
+
+
+app.post('/api/shorturl/new', function (req, res) {
+  userUrl = req.body.url;
+
+  var url = extractHostname(userUrl);
+
+  var w3 = dns.lookup(url, function (err, addresses, family) {
+    console.log(addresses);
+  });
+})
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+});
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    
+db.once('open', function () {
+  console.log("connected!")
 });
- 
-
-
-
-
 
 
 
